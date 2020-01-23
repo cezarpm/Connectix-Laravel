dd4181810479d19fa574085c41536841b531137d
Cezar <cezar_pm@hotmail.com>
2020-01-23 17:59:44 -0300

    created template.blade

---
+++ resources/views/template.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Index</title>
+    <link rel="stylesheet" href="/lib/bootstrap/dist/css/bootstrap.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/webfonts/all.css">
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img class="image-responsive" src="/img/logo.png" alt="" srcset="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="search-nav">
+                        <form class="form-inline">
+                            <i class="fas fa-search mr-2"></i>
+                            <input class="form-control" type="search" placeholder="Pesquise">
+                        </form>
+                    </div>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="btn btn-nav" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-nav" href="#">Registre-se</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <footer id="footer-section">
+        <div class="footer-copyright text-center">
+            <span>&copy; 2019 Connex, Inc.</span>
+        </div>
+    </footer>
+    <script src="/lib/jquery/dist/jquery.js"></script>
+    <script src="/lib/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <script src="/js/site.js"></script>
+</body>
+
+</html>
